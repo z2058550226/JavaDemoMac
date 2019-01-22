a5341607a761317cac2acf4cb440039de5642338
z2058550226 <2058550226@qq.com>
2019-01-22 15:24:13 +0800

    ordinary commit

--- src/main/java/com/suikajy/toolpro/tool/ByteArray2String.java
+++ src/main/java/com/suikajy/toolpro/tool/ByteArray2String.java
@@ -1,11 +1,9 @@
 package com.suikajy.toolpro.tool;
 
-import com.suikajy.toolpro.tool.String2ByteArray;
-
 public class ByteArray2String {
 
     public static void main(String[] args) {
-        byte[] byteArr = new byte[]{99, 104, 97, 110, 110, 101, 108};
+        byte[] byteArr = {109, 105, 115, 116, 102, 95, 97, 112, 107, 49, 57, 48, 49, 49, 54};
         System.out.println(new String(byteArr));
         System.out.println("asdf".substring(0,1));
         System.out.println(String2ByteArray.class.getCanonicalName());
--- src/main/java/com/suikajy/toolpro/tool/PackageNameGenTool.java
+++ src/main/java/com/suikajy/toolpro/tool/PackageNameGenTool.java
@@ -1,5 +1,8 @@
 package com.suikajy.toolpro.tool;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.util.Random;
 
 /**
@@ -18,5 +21,14 @@ public class PackageNameGenTool {
                     return sb.toString();
                 }).reduce("com", (a, b) -> a + "." + b);
         System.out.println(result);
+        setClipboardContents(result);
+        System.out.println("已复制到剪切板");
+    }
+
+    private static void setClipboardContents(String contents) {
+        StringSelection stringSelection = new StringSelection( contents );
+        // 系统剪贴板
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
     }
 }
